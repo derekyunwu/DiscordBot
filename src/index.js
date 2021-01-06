a6d09319d8195964e9642c5f6e52a3540146fe0e
Derek Wu <derekywu@berkeley.edu>
2021-01-06 13:16:19 -0800

    adding generate + help functionality

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 require('dotenv').config();
 
-const {Client, Message} = require('discord.js');
+const {Client, Message, MessageEmbed} = require('discord.js');
+const fetch = require('node-fetch');
 const bot = new Client();
 const PREFIX = "!";
+const url = "https://pokeapi.co/api/v2/pokemon/";
+const artwork = ""
 
 bot.login(process.env.DISCORDJS_BOT_TOKEN);
 
@@ -10,7 +13,7 @@ bot.on('ready', () => {
     console.log('Derkbot is online.');
 });
 
-bot.on('message', msg => {
+bot.on('message', async msg => {
     if (msg.author.bot) return;
     if (msg.content.startsWith(PREFIX)){
         // user is attempting to use bot functionality
@@ -23,6 +26,30 @@ bot.on('message', msg => {
             msg.channel.send("Command read as !intro");
         } else if (CMD_NAME === "kick"){
             msg.channel.send("Command read as !kick");
+        } else if (CMD_NAME === "generate"){
+            // testing the http requests
+            const pokemon_id = Math.floor( (Math.random() * 151) + 1);
+            const final_url = url + pokemon_id;
+
+            const { species , sprites } = await fetch(final_url)
+                .then(res => res.json())
+                .catch(err => {
+                    msg.channel.send("An error has occurred while retrieving Pokemon data.")
+                });
+            const artwork = sprites.other["official-artwork"].front_default;
+
+            // testing the embed
+            const pokemon_embed = new MessageEmbed()
+                .setColor('#0099ff')
+                .setTitle('A wild Pokemon has appeared!')
+                .setDescription('To catch this Pokemon, type !catch <pokemon-name>. The first person who guesses the correct Pokemon name catches the Pokemon successfully.')
+                .setImage(artwork)
+                .setTimestamp();
+            msg.channel.send(pokemon_embed);
+        } else if (CMD_NAME === "help"){
+            msg.channel.send(`
+            I support the following commands:\n\n**!help** - Displays my active commands\n**!intro** - Displays intro message for my activities\n**!generate** - Randomly returns an image of a Gen 1 Pokemon
+            `)
         }
     }
 });
