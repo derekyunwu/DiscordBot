d0caaa5fde1c7e7cd013ed2fcf5f4e079ab88cec
Derek Wu <derekywu@berkeley.edu>
2021-01-08 18:23:38 -0800

    updated intro and help commands to support extra functionality, added end function so that people can purge their data if wanted

--- src/index.js
+++ src/index.js
@@ -35,7 +35,26 @@ bot.on('message', async msg => {
         if (CMD_NAME === 'intro'){
 
             // display intro bot documentation
-            msg.channel.send("Command read as !intro");
+            msg.channel.send(`Hi ${author}! I'm DerkBot.\n` + 
+            'I am a PokeCord mini-clone. Through me, users in this server can catch + collect Pokemon. ' +
+            'Currently, I only support the following functionality: \n\n' +
+            '1) Spawning + Catching Pokemon\n' + 
+            '2) Rolling for Pokemon through an REM using dupe credits\n\n' +
+            'For more information, type <!help>.'
+            )
+
+        } else if (CMD_NAME === "help"){
+
+            msg.channel.send("I support the following commands:\n\n" +
+            "**!help** - Displays my active commands\n" + 
+            "**!intro** - Displays a welcome description for new users\n" + 
+            "**!start** - Claims **Pokemon Trainer** role / Can begin catching Pokemon\n" + 
+            "**!generate** - Randomly returns an image of a Gen 1 Pokemon\n" + 
+            "**!catch <pokemon-name>** - Adds pokemon to personal inventory if <pokemon-name> is correct / Can only be used if a wild Pokemon has appeared\n" + 
+            "**!inventory** - Prints a list of Pokemon in user inventory\n" + 
+            "**!rolls** - Prints the number of rolls + credits owned by a user\n" +
+            "**!end** - Forfeit **Pokemon Trainer** role / All data is purged\n" 
+            );
 
         } else if (CMD_NAME === "start"){
             //begins pokemon adventure
@@ -57,8 +76,8 @@ bot.on('message', async msg => {
 
             const role = guild.roles.cache.find(role => role.name === roleName);
             member.roles.add(role);
+            msg.channel.send(`Congratulations ${author}! You have begun your Pokemon adventure. You have claimed the ${roleName} role.`)
 
-            msg.channel.send(`Congratulations ${author}! You have begun your Pokemon adventure.`)
         } else if (CMD_NAME === "generate"){
 
             // testing the http requests
@@ -82,18 +101,6 @@ bot.on('message', async msg => {
             
             msg.channel.send(pokemon_embed);
             curr_pokemon = species.name.charAt(0).toUpperCase() + species.name.slice(1); // may not be the best way to do this
-
-        } else if (CMD_NAME === "help"){
-
-            msg.channel.send("I support the following commands:\n\n" +
-            "**!help** - Displays my active commands\n" + 
-            "**!start** - Claims **Pokemon Trainer** role / Can begin catching Pokemon\n" + 
-            "**!generate** - Randomly returns an image of a Gen 1 Pokemon\n" + 
-            "**!catch <pokemon-name>** - Adds pokemon to personal inventory if <pokemon-name> is correct / Can only be used if a wild Pokemon has appeared\n" + 
-            "**!inventory** - Prints a list of Pokemon in user inventory\n" + 
-            "**!rolls** - Prints the number of rolls + credits owned by a user\n" +
-            "**!end** - Forfeit **Pokemon Trainer** role / All data is purged\n" 
-            );
 
         } else if (CMD_NAME === "catch"){
 
@@ -159,11 +166,34 @@ bot.on('message', async msg => {
                 })
                 
             }
+        } else if (CMD_NAME === "rolls") {
+
+            msg.channel.send(`Command read as !rolls`)
+
         } else if (CMD_NAME === "end"){
+            // if does not have pokemon trainer role
+            if (!member.roles.cache.some(role => role.name === roleName)){
+                msg.channel.send(`${author}, it looks like you haven't started your Pokemon adventure. Type **!start** to begin catching Pokemon.`);
+            } else {
 
-            msg.channel.send('Command read as !end')
+                await mongo().then(async mongoose => {
+                    try {
+                        await userInvenSchema.findOneAndDelete({
+                            _id: id
+                        })
+                        // remove the role from the user
 
-        }
+                        const role = guild.roles.cache.find(role => role.name === roleName);
+                        member.roles.remove(role);
+
+                    } finally {
+                        mongoose.connection.close()
+                    }
+                })
+                
+                msg.channel.send(`${author} your data has been deleted and cannot be recovered.`)
+            }
+        } 
     }
 });
 
